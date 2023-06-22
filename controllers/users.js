6db8491cfdaf4c15bdc306286c1561350be50aca
Bryan Lynn Welton III <weltontrain@gmail.com>
2023-06-22 09:34:51 -0600

    committed user statements

--- controllers/users.js
+++ controllers/users.js
@@ -1,1 +1,87 @@
 const Users = require('../models/users');
+
+const getAllUsers = async (req, res) => {
+    try {
+        const result = await Users.find();
+        res.status(200).json(result);
+    } catch (error) {
+        res
+            .status(500)
+            .json({ message: 'Error retrieving Users', error: error.message });
+    }
+}
+
+const getUser = async (req, res) => {
+    try {
+        const userId = new ObjectId(req.params.id);
+        const result = await Users.find(userId);
+        res.status(200).json(result);
+    } catch (error) {
+        res
+            .status(500)
+            .json({ message: 'Error retrieving User', error: error.message });
+    }
+}
+
+const createUser = async (req, res) => {
+    try {
+        const { email, shippingAddress, billingAddress, paymentMethods } = req.body;
+        
+        const newUser = new Users({ email, shippingAddress, billingAddress, paymentMethods });
+
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch (error) {
+        res
+            .status(500)
+            .json({ message: 'Error creating User', error: error.message });
+    }
+};
+
+const updateUser = async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const User = {
+            email: req.body.email,
+            shippingAddress: req.body.shippingAddress,
+            billingAddress: req.body.billingAddress,
+            paymentMethods: req.body.paymentMethods
+        };
+    
+        const response = await Users.findByIdAndUpdate(userId, User, { new: true });
+    
+        if (!response) {
+            res.status(404).json({ error: 'User not found' });
+            return;
+        }
+        res.status(204).json(response);
+    } catch (error) {
+        res
+            .status(500)
+            .json({ message: 'Error updating User', error: error.message });
+    }
+};
+
+const deleteUser = async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const response = await Users.findByIdAndDelete(userId);
+        if (!response) {
+            res.status(404).json({ error: 'User not found' });
+            return;
+        }
+        res.status(204).json(response);
+    } catch (error) {
+        res
+            .status(500)
+            .json({ message: 'Error deleting User', error: error.message });
+    }
+};
+
+module.exports = {
+  getAllUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser
+};
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,19 @@
 const router = require('express').Router();
-// const {
-//   getAllUsers,
-//   getUser,
-//   createUser,
-//   updateUser,
-//   deleteUser,
-// } = require('../controllers/orders');
+
+const {
+  getAllUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('../controllers/users');
+
 const { requiresAuth } = require('express-openid-connect');
 
-// router.get('/', );
-// router.get('/:id', );
-// router.post('/', requiresAuth, );
-// router.put('/:id', requiresAuth, );
-// router.delete('/:id', requiresAuth, );
+router.get('/', getUser);
+router.get('/:id', getAllUsers);
+router.post('/', requiresAuth, createUser);
+router.put('/:id', requiresAuth, updateUser);
+router.delete('/:id', requiresAuth, deleteUser);
 
 module.exports = router;
