b2ec1ac505936e480246e0615534b9ff5d8a5303
Bentleyr98 <bentley.rebekah314@gmail.com>
2023-06-24 14:31:44 -0600

    cors

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
   .use('/', require('./routes'));
