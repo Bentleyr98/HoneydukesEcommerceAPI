6430429954eda51ff4cd69ef439a7e9d53afdb65
Bryan Lynn Welton III <weltontrain@gmail.com>
2023-06-24 08:38:15 -0600

    Updated requiresAuth to have ()

--- routes/users.js
+++ routes/users.js
@@ -12,8 +12,8 @@ const { requiresAuth } = require('express-openid-connect');
 
 router.get('/', getUser);
 router.get('/:id', getAllUsers);
-router.post('/', requiresAuth, createUser);
-router.put('/:id', requiresAuth, updateUser);
-router.delete('/:id', requiresAuth, deleteUser);
+router.post('/', requiresAuth(), createUser);
+router.put('/:id', requiresAuth(), updateUser);
+router.delete('/:id', requiresAuth(), deleteUser);
 
 module.exports = router;
