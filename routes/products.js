4d2aa037664e6c3a0610339dd4902be97f1ddb0e
SamiThueson <wea16002@byui.edu>
2023-06-23 22:08:24 -0600

    Fixed oauth on routes

--- routes/products.js
+++ routes/products.js
@@ -4,8 +4,8 @@ const { requiresAuth } = require('express-openid-connect');
 
 router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProduct);
-router.post('/', requiresAuth, productController.createProduct);
-router.put('/:id', requiresAuth, productController.updateProduct);
-router.delete('/:id', requiresAuth, productController.deleteProduct);
+router.post('/', requiresAuth(), productController.createProduct);
+router.put('/:id', requiresAuth(), productController.updateProduct);
+router.delete('/:id', requiresAuth(), productController.deleteProduct);
 
 module.exports = router;
