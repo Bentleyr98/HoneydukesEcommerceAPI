09663faac8b0b41869562a2bf422810c512faa2e
Bentleyr98 <bentley.rebekah314@gmail.com>
2023-06-28 22:40:45 -0600

    Review routes completed

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,17 +1,13 @@
 const Reviews = require('../models/reviews');
 
-
 const getAllReviews = async (req, res, next) => {
     try {
         const reviews = await Reviews.find();
         res.status(200).json(reviews);
       } catch (err) {
-        res
-          .status(500)
-          .json({ message: "Error retrieving all reviews", error: err.message });
+        res.status(500).json({ message: "Oh no! Looks like we lost all the reviews...", error: err.message });
       }
     }
-
 
 async function getReview(req, res) {
     try {
@@ -20,26 +16,65 @@ async function getReview(req, res) {
         if (review) {
             res.status(200).json(review);
         } else {
-            return res.status(404).json({message: "Review not found"});
+            return res.status(404).json({ message: "404!! Review not found" });
         }
-        
     } catch (err) {
-        res.status(500)
-        .json({ message:"Error retrieving review", error: err.message});
+        res.status(500).json({ message:"Error retrieving review. Someone must've removed it.", error: err.message });
     }
     }
 
 const createReview = async (req, res, next) => {
-    return
+    try {
+        const { userEmail, productID, reviewText, rating, date, otherDetails } = req.body;
+        const newReview = new Reviews({
+          userEmail,
+          productID,
+          reviewText,
+          rating,
+          date,
+          otherDetails
+        });
+        const savedReview = await newReview.save();
+        res.status(201).json(savedReview);
+      } catch (err) {
+        res.status(500).json({ message: 'Problem creating review.', error: err.message });
+      }
     }
 
-
 const updateReview = async (req, res, next) => {
-    return
+    try {
+        const review_id = req.params.id;
+        const updatedReview = {
+          userEmail: req.body.userEmail,
+          productID: req.body.productID,
+          reviewText: req.body.reviewText,
+          rating: req.body.rating,
+          date: req.body.date,
+          otherDetails: req.body.otherDetails
+        };
+        const review = await Reviews.findByIdAndUpdate(review_id, updatedReview, {new: true});
+        if (!review) {
+          res.status(404).json({ error: '404 Review not found' });
+          return;
+        } else {
+          res.status(204).json(review);
+        }
+      } catch (err) {
+        res.status(500).json({ message: "Uh oh. Can't update that review.", error: err.message });
+      }
     }
 
 const deleteReview = async (req, res, next) => {
-    return
+    try {
+        const review_id = req.params.id;
+        const deletedReview = await Reviews.findByIdAndDelete(review_id);
+        if (!deletedReview) {
+          return res.status(404).json({ message: 'Review not found' });
+        }
+        res.status(200).json({ message: 'Review deleted successfully' });
+      } catch (err) {
+        res.status(500).json({ message: 'Error deleting review', error: err.message });
+      }
     }
 
-module.exports = {getAllReviews, getReview, createReview, updateReview, deleteReview }
+module.exports = { getAllReviews, getReview, createReview, updateReview, deleteReview }
