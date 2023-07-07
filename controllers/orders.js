d2fb5313f15bbc5ee1a73ec6c54aafefd5372abc
torgill43 <104460644+torgill43@users.noreply.github.com>
2023-07-06 22:15:05 -0600

    all routes complete

--- controllers/orders.js
+++ controllers/orders.js
@@ -2,6 +2,7 @@ const Orders = require("../models/orders");
 
 // Get all orders
 async function getAllOrders(req, res) {
+  // #swagger.tags= ['Orders']
   try {
     const orders = await Orders.find();
     res.status(200).json(orders);
@@ -13,6 +14,7 @@ async function getAllOrders(req, res) {
 }
 
 async function getOrder(req, res) {
+  // #swagger.tags= ['Orders']
   try {
     const orderId = req.params.id;
     const order = await Orders.findById(orderId);
@@ -28,7 +30,99 @@ async function getOrder(req, res) {
   }
 }
 
+async function createOrder(req, res) {
+  // #swagger.tags= ['Orders']
+  try {
+    const {
+      userEmail,
+      orderDate,
+      shippingAddress,
+      billingAddress,
+      paymentMethod,
+      orderStatus,
+      productID,
+      quantity,
+      price,
+      additionalDetails,
+    } = req.body;
+
+    const newOrder = new Orders({
+      userEmail,
+      orderDate,
+      shippingAddress,
+      billingAddress,
+      paymentMethod,
+      orderStatus,
+      orderedProducts: [
+        {
+          productID,
+          quantity,
+          price,
+        },
+      ],
+      additionalDetails,
+    });
+
+    const savedOrder = await newOrder.save();
+    res.status(201).json(savedOrder);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error creating order", error: err.message });
+  }
+}
+
+async function updateOrder(req, res) {
+  // #swagger.tags= ['Orders']
+  try {
+    const orderId = req.params.id;
+    const updatedOrder = {
+      userEmail: req.body.userEmail,
+      orderDate: req.body.orderDate,
+      shippingAddress: req.body.shippingAddress,
+      billingAddress: req.body.billingAddress,
+      paymentMethod: req.body.paymentMethod,
+      orderStatus: req.body.orderStatus,
+      productID: req.body.productID,
+      quantity: req.body.quantity,
+      price: req.body.price,
+      additionalDetails: req.body.additionalDetails,
+    };
+
+    const order = await Orders.findByIdAndUpdate(orderId, updatedOrder, {
+      new: true,
+    });
+
+    if (!order) {
+      res.status(404).json({ err: "Order not found" });
+      return;
+    } else {
+      res.status(204).json(order);
+    }
+  } catch (err) {
+    res.status(500).json({ err: "Error updating order" });
+  }
+}
+
+async function deleteOrder(req, res) {
+  // #swagger.tags= ['Orders']
+  try {
+    const orderId = req.params.id;
+    const deletedOrder = await Orders.findByIdAndDelete(orderId);
+    if (!deletedOrder) {
+      res.status(404).json({ message: "Product not found" });
+      return;
+    }
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Error deleting order", err: err.message });
+  }
+}
+
 module.exports = {
   getAllOrders,
   getOrder,
+  createOrder,
+  updateOrder,
+  deleteOrder,
 };
--- routes/orders.js
+++ routes/orders.js
@@ -2,16 +2,16 @@ const router = require("express").Router();
 const {
   getAllOrders,
   getOrder,
-  //   createOrder,
-  //   updateOrder,
-  //   deleteOrder,
+  createOrder,
+  updateOrder,
+  deleteOrder,
 } = require("../controllers/orders");
 const { requiresAuth } = require("express-openid-connect");
 
 router.get("/", getAllOrders);
 router.get("/:id", getOrder);
-// router.post('/', requiresAuth, );
-// router.put('/:id', requiresAuth, );
-// router.delete('/:id', requiresAuth, );
+router.post("/", requiresAuth(), createOrder); // requiresAuth() DON'T FORGET
+router.put("/:id", requiresAuth(), updateOrder); // requiresAuth() DON'T FORGET
+router.delete("/:id", requiresAuth(), deleteOrder); // requiresAuth() DON'T FORGET
 
 module.exports = router;
